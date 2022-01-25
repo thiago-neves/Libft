fee680175e340e487802394b5ac28624e77730f7
tneves <tneves@student.42sp.org.br>
2022-01-25 12:22:35 -0300

    added files

---
+++ src/ft_atoi.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tneves <tneves@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/03/12 11:01:02 by tneves            #+#    #+#             */
+/*   Updated: 2020/09/21 15:06:06 by tneves           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int				ft_atoi(const char *str)
+{
+	long	number;
+	int		sign;
+
+	sign = 1;
+	number = 0;
+	while ((*str >= 9 && *str <= 13) || *str == 32)
+		str++;
+	if (*str == '+' && *(str + 1) == '-')
+		return (0);
+	if (*str == '+')
+		str++;
+	if (*str == '-')
+	{
+		sign = -1;
+		str++;
+	}
+	while (*str >= '0' && *str <= '9')
+	{
+		number = number * 10 + (*str - '0');
+		str++;
+	}
+	if (number < 0)
+		return ((sign > 0) ? -1 : 0);
+	return (sign * number);
+}
